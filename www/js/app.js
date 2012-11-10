3b961d74e83ddcd93e8493ac157c89e2fcc97acd
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-11-10 15:50:02 -0600

    Create a way to clear the current views for the new one

--- www/js/app.js
+++ www/js/app.js
@@ -49,7 +49,13 @@ define("app", ['backbone', 'install', 'localstorage'], function(Backbone, instal
             "add": "addNote",
             ".*": "viewNotes"
         },
+        switchView: function(id) {
+          console.log('switching...' , id);
+          $('#pages .page').hide();
+          $('#' + id).show();
+        },
         viewNotes: function() {
+          this.switchView('viewNotes');
           var Note = Backbone.Model.extend({});
 
           var NoteList = Backbone.Collection.extend({
@@ -110,19 +116,10 @@ define("app", ['backbone', 'install', 'localstorage'], function(Backbone, instal
           noteListView.render();
         },
         addNote: function() {
+          this.switchView('addNote');
           alert('hell yeah!');
         }
     });
-
-    var note = new Note({text: "garage: 1122", position: {lat: 1, lng: 1}});
-    notes.add(note);
-    var note = new Note({text: "buy airlock", position: {lat: 1, lng: 1}});
-    notes.add(note);
-    var note = new Note({text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", position: {lat: 1, lng: 1}});
-    notes.add(note);
-
-    var noteListView = new NoteListView({collection: notes});
-    noteListView.render();
 
     var app_router = new AppRouter();
 
